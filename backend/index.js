cb653d4e9a0a1c10c23d2c888c2d2c6eef91f069
Taylor <taybart@gmail.com>
2018-02-23 18:34:46 -0700

    Inital api with LUT and body type assets

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
+const PORT = 3001;
 
 const app = express();
+app.use(cors());
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => {
+  res.json({ status: 'OK', msg: 'Hello World!' });
+});
+app.get('/ghosts', (req, res) => {
+  res.sendFile(`/assets/ghosts/${req.query.bt}.png`, { root: __dirname });
+});
+app.get('/item-lut', (req, res) => {
+  res.json({ status:'OK', data: { 0: 'Nothing' } });
+});
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
